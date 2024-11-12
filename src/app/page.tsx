4088a3153843a5212acde7b94094ae47cb7064db
abdibrokhim <abdibrokhim@gmail.com>
2024-11-12 18:07:44 +0500

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans bg-[#212121e6] text-white">
+    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans bg-[#212121e6] text-white old-paper-bg">
       <Analytics />
       {/* Logo */}
       <header className="row-start-1 w-full flex justify-between items-center">
